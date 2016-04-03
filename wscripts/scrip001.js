ec8cf4483fac5a183ee0e96fddb2db7457b219c4
SergeyRykov <rykcer@mail.ru>
2016-04-03 19:56:38 +0600

    коммит от 3-го апреля воскресенье

--- wscripts/scrip001.js
+++ wscripts/scrip001.js
@@ -1,12 +1,5 @@
-var id_nam = document.getElementById("nam").value;
-var id_fam = document.getElementById("fam");
-var id_gen = document.getElementById("gen");
-var id_bun = document.getElementById("bun");
-var id_plc = document.getElementById("plc");
-var id_cit = document.getElementById("cit");
-
-// var id_show_but = document.getElementById("butshow");
-
+var gl_name = '';
+var ktoto = {};
 var s_data = {
     givenName: '',
     firstName: '',
@@ -14,40 +7,61 @@ var s_data = {
     dataB: '',
     placeB: '',
     citizen: '',
-    Fillup: function (a,b,c,d,e,h){
+    Fillup: function(a, b, c, d, e, h) {
         this.givenName = a;
         this.firstName = b;
-        this.gender    = c;
-        this.dataB     = d;
-        this.placeB    = e;
-        this.citizen   = h;
+        this.gender = c;
+        this.dataB = d;
+        this.placeB = e;
+        this.citizen = h;
     },
-    Fullname: function () {
-        return this.firstName+' '+this.givenName;
+    Fullname: function() {
+        return this.firstName + ' ' + this.givenName;
     }
-}
+};
+
 // отображение заполненных данных
 function ShowObject(ob) {
     var names = "";
-    for(var name in ob) names += ob[name] + " | ";
+    for (var name in ob) names += ob[name] + "\n";
     return names;
     // console.log(Object.keys(ob));
     // return Object.keys(ob);
 }
 
-// - - - - заполнение структуры - - - 
-var ktoto = s_data;
-ktoto.Fillup(id_nam, 
-             id_fam.value,
-             id_gen.value,
-             id_bun.value,
-             id_plc.value,
-             id_cit.valuet
-    );
-
-ShowObject(ktoto);
-console.log(ktoto.Fullname());
-alert(ShowObject(ktoto));
-
 // alert('Для просмотра доп сообщений запустите консоль.');
 console.log('[LOG: is finished!]');
+
+window.onload = InitDo;
+
+function GoFill() {
+    var Userform = document.getElementById("userform");
+    s_data.Fillup(Userform.nam.value,
+        Userform.fam.value,
+        Userform.gen.value,
+        Userform.bun.value,
+        Userform.plc.value,
+        Userform.cit.value
+    );
+}
+
+function Bshow() {
+    GoFill();
+    alert('Вас зовут: ' + s_data.Fullname());
+}
+
+function Bfull() {
+    GoFill();
+    gl_name = ShowObject(s_data);
+    alert('Значение: ' + gl_name);
+}
+
+function InitDo() {
+
+    var ButonSh = document.getElementById('userform');
+    ButonSh.butshow.onclick = Bshow;
+
+    var ButonFil = document.getElementById('userform');
+    ButonFil.butful.onclick = Bfull;
+
+}
